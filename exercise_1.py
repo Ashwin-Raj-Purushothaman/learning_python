48bb2c817049772acda0e8820075e7b0f52f248f
Ashwin-Raj-Purushothaman <ash@cobuild.tech>
2025-07-26 18:56:42 +0530

    Add files via upload

---
+++ exercise_1.py
@@ -0,0 +1,6 @@
+print ("hello world")
+print ("hello Again")
+print ("Python is Fun")
+print ('Coding in Python')
+print ("Printing Done")
+
---
+++ exercise_2.py
@@ -0,0 +1,6 @@
+#comments
+# anything after # is ignored
+
+print ("This print statement is 'Not commented' ")
+
+# print("this will not run")
